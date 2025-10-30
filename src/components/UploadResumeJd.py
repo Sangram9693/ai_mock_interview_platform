df5240c4b7bbddaf35a41c9dc08e5bb40f208262
Lalatendu Jena <lalatendujena007@gmail.com>
2025-10-30 21:46:50 +0530

    Lalatendu fix (#2)
    * Design Interview Page
    
    * Desgin Inteview Pagew
    
    * StateManager Change
    
    * .idea comment
    
    * Utility Change
    
    ---------
    
    Co-authored-by: Lalatendu Jena <lalatendujena@Lalatendus-MacBook-Air.local>

--- src/components/UploadResumeJd.py
+++ src/components/UploadResumeJd.py
@@ -24,6 +24,7 @@ def render(navigate):
 
         if Button("Start Interview", disabled=is_disable):
             nevigate_component(navigate, "interview")
-            
+
+
 
         
--- src/utils/StateManager.py
+++ src/utils/StateManager.py
@@ -1,15 +1,13 @@
 import streamlit as st
 
+
 def init_state():
+    """Initialize core app states."""
     defaults = {
         "page": "upload",
         "job_details": {},
         "interview_data": {},
     }
     for key, value in defaults.items():
-        if key not in st.session_state:
-            st.session_state[key] = value
+        st.session_state.setdefault(key, value)
 
-def reset_interview():
-    st.session_state.job_details = {}
-    st.session_state.interview_data = {}
